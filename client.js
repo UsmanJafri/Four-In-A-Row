e630b9a490b20205d9a83d211497cdaf0afc4ff5
Usman Jafri <usmanjafri@hotmail.com>
2018-05-27 02:11:49 +0500

    initializing repo

---
+++ client.js
@@ -0,0 +1,168 @@
+const socket = io()
+const state = {}
+
+const squaresInit = () => {
+    const squares = []
+    const char = "'"
+    for(i = 0;i < 6 * 7;i++) {
+        squares.push(char)
+    }
+    return squares
+}
+
+const ind = (r,c) => (r * 7) + c
+
+const checkEnemyWin = () => {
+    for (r = 0;r < 3;r++) {
+        for (c = 0;c < 7;c++) {
+            if (state.squares[ind(r,c)] == "'" && state.squares[ind(r+1,c)] == state.squares[ind(r+2,c)] && state.squares[ind(r+2,c)] == state.squares[ind(r+3,c)] && state.squares[ind(r+3,c)] == state.enemySymbol) {
+                return c
+            }
+        }
+    }
+    for (r = 0;r < 6;r++) {
+        for (c = 0;c < 4;c++) {
+            let fourInRow = state.squares[ind(r,c)] + state.squares[ind(r,c+1)] + state.squares[ind(r,c+2)] + state.squares[ind(r,c+3)]
+            if (state.enemySymbol == 'X') {
+                if (fourInRow == "XXX'") {return (c + 3)}
+                else if (fourInRow == "XX'X") {return (c + 2)}
+                else if (fourInRow == "X'XX") {return (c + 1)}
+                else if (fourInRow == "'XXX") {return c}
+            }
+            else if (state.enemySymbol == 'O') {
+                if (fourInRow == "OOO'") {return (c + 3)}
+                else if (fourInRow == "OO'O") {return (c + 2)}
+                else if (fourInRow == "O'OO") {return (c + 1)}
+                else if (fourInRow == "'OOO") {return c}
+            }
+        }
+    }
+    for (r = 0;r < 3;r++) {
+        for (c = 0;c < 4;c++) {
+            let fourInRow = state.squares[ind(r,c)] + state.squares[ind(r+1,c+1)] + state.squares[ind(r+2,c+2)] + state.squares[ind(r+3,c+3)]
+            if (state.enemySymbol == 'X') {
+                if (fourInRow == "XXX'") {return (c + 3)}
+                else if (fourInRow == "XX'X") {return (c + 2)}
+                else if (fourInRow == "X'XX") {return (c + 1)}
+                else if (fourInRow == "'XXX") {return c}
+            }
+            else if (state.enemySymbol == 'O') {
+                if (fourInRow == "OOO'") {return (c + 3)}
+                else if (fourInRow == "OO'O") {return (c + 2)}
+                else if (fourInRow == "O'OO") {return (c + 1)}
+                else if (fourInRow == "'OOO") {return c}
+            }
+        }
+    }
+    for (r = 2;r < 6;r++) {
+        for (c = 3;c < 7;c++) {
+            let fourInRow = state.squares[ind(r,c)] + state.squares[ind(r+1,c-1)] + state.squares[ind(r+2,c-2)] + state.squares[ind(r+3,c-3)]
+            if (state.enemySymbol == 'X') {
+                if (fourInRow == "XXX'") {return (c + 3)}
+                else if (fourInRow == "XX'X") {return (c + 2)}
+                else if (fourInRow == "X'XX") {return (c + 1)}
+                else if (fourInRow == "'XXX") {return c}
+            }
+            else if (state.enemySymbol == 'O') {
+                if (fourInRow == "OOO'") {return (c + 3)}
+                else if (fourInRow == "OO'O") {return (c + 2)}
+                else if (fourInRow == "O'OO") {return (c + 1)}
+                else if (fourInRow == "'OOO") {return c}
+            }
+        }
+    }
+    return -1
+}
+
+const moveUpdate = (event,c) => {
+    if (state.game && state.turn) {
+        if (state.columnSize[c] >= 6) {
+            ReactDOM.render(React.createElement('h4',null,'STATUS: Selected Column is full. Choose another column'),document.getElementById('status'))
+        }
+        else {
+            socket.emit('turnPlayed',c,state.gameId)
+            setState({turn: false})
+        }
+    }
+    else if (!state.game || (state.game && !state.turn)) {
+    }
+}
+
+const hoverInUpdate = (v,c) => {
+    const squaresList = state.squares.map((d,i) => React.createElement('button',{style: {'max-width': '24px',padding: '10px 24px'},key: i,value: i,'data-column': i%7,onClick: (e) => moveUpdate(e,e.target.dataset.column),onMouseOver: (e)=> hoverInUpdate(e.target.value,e.target.dataset.column),onMouseOut: (e)=> setState()},state.squares[i]))
+    for (r = 0;r<6;r++) {
+        let hoverColor = 'green'
+        // if (state.enemyWin != -1 && state.enemyWin != c) {
+        if (state.enemyWin != -1) {
+            hoverColor = 'red'
+        }
+        squaresList[(r * 7) + (c*1)].props.style.backgroundColor=hoverColor
+    }
+    ReactDOM.render(
+        (document.title = "Connect-4",
+        React.createElement('div',{id: 'myGrid'},
+            React.createElement('h2',null,"Welcome to Connect-4"),
+            React.createElement('div',{id: 'p1'},React.createElement('h4',null,"X - Not Connected")),
+            React.createElement('div',{id: 'p2'},React.createElement('h4',null,"O - Not Connected")),
+            React.createElement('div',{id: 'status'},React.createElement('h4',null,"STATUS:")),
+            React.createElement('div',{id: 0},squaresList.slice(0,7)),
+            React.createElement('div',{id: 1},squaresList.slice(7,14)),
+            React.createElement('div',{id: 2},squaresList.slice(14,21)),
+            React.createElement('div',{id: 3},squaresList.slice(21,28)),
+            React.createElement('div',{id: 4},squaresList.slice(28,35)),
+            React.createElement('div',{id: 5},squaresList.slice(35,42))
+        )
+    ),document.getElementById('root'))
+}
+
+const setState = updates => {
+    Object.assign(state,updates)
+    const squaresList = state.squares.map((d,i) => React.createElement('button',{style: {'max-width': '24px',padding: '10px 24px'},key: i,value: i,'data-column': i%7,onClick: (e) => moveUpdate(e,e.target.dataset.column),onMouseOver: (e)=> hoverInUpdate(e.target.value,e.target.dataset.column),onMouseOut: (e)=> setState()},state.squares[i]))
+    ReactDOM.render(
+        (document.title = "Connect-4",
+        React.createElement('div',{id: 'myGrid'},
+            React.createElement('h2',null,"Welcome to Connect-4"),
+            React.createElement('div',{id: 'p1'},React.createElement('h4',null,"X - Not Connected")),
+            React.createElement('div',{id: 'p2'},React.createElement('h4',null,"O - Not Connected")),
+            React.createElement('div',{id: 'status'},React.createElement('h4',null,"STATUS:")),
+            React.createElement('div',{id: 0},squaresList.slice(0,7)),
+            React.createElement('div',{id: 1},squaresList.slice(7,14)),
+            React.createElement('div',{id: 2},squaresList.slice(14,21)),
+            React.createElement('div',{id: 3},squaresList.slice(21,28)),
+            React.createElement('div',{id: 4},squaresList.slice(28,35)),
+            React.createElement('div',{id: 5},squaresList.slice(35,42))
+        )
+    ),document.getElementById('root'))
+}
+
+socket.on('playerSym',data => {
+    state.symbol = data
+    if (data == 'X') {
+        state.enemySymbol = 'O'
+        ReactDOM.render(React.createElement('h4',null,"X - Me"),document.getElementById('p1'))
+    }
+    else if (data == 'O') {
+        state.enemySymbol = 'X'
+        ReactDOM.render(React.createElement('h4',null,"O - Me"),document.getElementById('p2'))
+    }
+})
+socket.on('status',data => {
+    ReactDOM.render(React.createElement('h4',null,'STATUS: '+data),document.getElementById('status'))
+})
+socket.on('gameStart',gameId => {
+    state.gameId = gameId
+    if (state.symbol == 'X') {
+        ReactDOM.render(React.createElement('h4',null,"O - Guest"),document.getElementById('p2'))
+    }
+    else if (state.symbol == 'O') {
+        ReactDOM.render(React.createElement('h4',null,"X - Guest"),document.getElementById('p1'))
+    }
+    state.game = true
+})
+socket.on('turn',() => setState({turn: true}))
+socket.on('squaresUpdate',(newSquares,newColSizes) => {
+    setState({squares: newSquares,columnSize: newColSizes})
+    state.enemyWin = checkEnemyWin()
+})
+
+setState({gameId: '',squares: squaresInit(),columnSize: [0,0,0,0,0,0,0],game: false,turn: false,symbol: '',enemySymbol: '',enemyWin: -1})
---
+++ server.js
@@ -0,0 +1,154 @@
+const fs = require('fs')
+const http = require("http")
+const socketio = require('socket.io')
+
+states = []
+playerCount = 0
+const server = http.createServer(async (req,resp) => resp.end(await readfile(req.url.substr(1))))
+const io = socketio(server)
+server.listen(8000,() => console.log("Four-In-A-Row Server Running"))
+
+const readfile = f => new Promise((resolve,reject) => fs.readFile(f,(e,d) => e?reject(e):resolve(d)))
+
+const ind = (r,c) => (r * 7) + c
+
+const squaresInit = () => {
+    const squares = []
+    const char = "'"
+    for (i = 0;i < 6 * 7;i++) {
+        squares.push(char)
+    }
+    return squares
+}
+
+const checkWin = (sID) => {
+    const state = states[sID]
+    if (state.columnSize.reduce((x,y) => x + y) == 42) {
+        return -1
+    }
+    for (r = 0;r < 3;r++) {
+        for (c = 0;c < 7;c++) {
+            let fourInRow = state.squares[ind(r,c)] + state.squares[ind(r+1,c)] + state.squares[ind(r+2,c)] + state.squares[ind(r+3,c)]
+            if (fourInRow == 'XXXX' || fourInRow == 'OOOO') {
+                return 1
+            }
+        }
+    }
+    for (r = 0;r < 6;r++) {
+        for (c = 0;c < 4;c++) {
+            let fourInRow = state.squares[ind(r,c)] + state.squares[ind(r,c+1)] + state.squares[ind(r,c+2)] + state.squares[ind(r,c+3)]
+            if (fourInRow == 'XXXX' || fourInRow == 'OOOO') {
+                return 1
+            }
+        }
+    }
+    for (r = 0;r < 3;r++) {
+        for (c = 0;c < 4;c++) {
+            let fourInRow = state.squares[ind(r,c)] + state.squares[ind(r+1,c+1)] + state.squares[ind(r+2,c+2)] + state.squares[ind(r+3,c+3)]
+            if (fourInRow == 'XXXX' || fourInRow == 'OOOO') {
+                return 1
+            }
+        }
+    }
+    for (r = 2;r < 6;r++) {
+        for (c = 3;c < 7;c++) {
+            let fourInRow = state.squares[ind(r,c)] + state.squares[ind(r+1,c-1)] + state.squares[ind(r+2,c-2)] + state.squares[ind(r+3,c-3)]
+            if (fourInRow == 'XXXX' || fourInRow == 'OOOO') {
+                return 1
+            }
+        }
+    }
+    return 0
+}
+
+const turnUpdate = (newTurn,outcome,sID) => {
+    if (outcome == 1) {
+        states[sID].notTurn.emit('status',"YOU LOST! :( New game started. Guest's turn, Please wait")
+        states[sID].turn.emit('status','YOU WON! :) New game started. Your turn')
+        states[sID] = {x: states[sID].x,o: states[sID].o,squares: squaresInit(),columnSize: [0,0,0,0,0,0,0],turn: states[sID].turn,turnSymbol: states[sID].turnSymbol,notTurn: states[sID].notTurn,notTurnSymbol: states[sID].notTurnSymbol}
+        newTurn = states[sID].turnSymbol
+        states[sID].x.emit('squaresUpdate',states[sID].squares,states[sID].columnSize)
+        states[sID].o.emit('squaresUpdate',states[sID].squares,states[sID].columnSize)
+    }
+    else if (outcome == -1) {
+        states[sID].x.emit('status','GAME DRAWN! New game started. Your turn')
+        states[sID].o.emit('status',"GAME DRAWN! New game started. Guest's turn, Please wait")
+        states[sID] = {x: states[sID].x,o: states[sID].o,squares: squaresInit(),columnSize: [0,0,0,0,0,0,0],turn: states[sID].x,turnSymbol: 'X',notTurn: states[sID].o,notTurnSymbol: 'O'}
+        newTurn = 'X'
+        states[sID].x.emit('squaresUpdate',states[sID].squares,states[sID].columnSize)
+        states[sID].o.emit('squaresUpdate',states[sID].squares,states[sID].columnSize)
+    }
+    states[sID].turnSymbol = newTurn
+    if (states[sID].turnSymbol == 'X') {
+        states[sID].turn = states[sID].x
+        states[sID].notTurn = states[sID].o
+        states[sID].notTurnSymbol = 'O'
+    }
+    else if (states[sID].turnSymbol == 'O') {
+        states[sID].turn = states[sID].o
+        states[sID].notTurn = states[sID].x
+        states[sID].notTurnSymbol = 'X'
+    }
+    if (outcome == 0) {
+        states[sID].notTurn.emit('status',"Guest's turn, Please wait")
+        states[sID].turn.emit('status','Your turn')
+    }
+    states[sID].turn.emit('turn')
+}
+
+io.sockets.on('connection',socket => {
+    let sID = states.length - 1
+    if (playerCount % 2 == 0) {
+        const newState = {squares: squaresInit(),columnSize: [0,0,0,0,0,0,0],x: '',o: '',turn: '',turnSymbol: 'u',notTurn: '',notTurnSymbol: ''}
+        states.push(newState)
+        sID = states.length - 1
+        console.log('State ID:',sID,', X Connected')
+        states[sID].x = socket
+        socket.emit('playerSym','X')
+        playerCount++
+        states[sID].x.emit('status','Waiting for O to connect')
+    }
+    else {
+        console.log('State ID:',sID,', O Connected')
+        states[sID].o = socket
+        socket.emit('playerSym','O')
+        playerCount++
+        states[sID].x.emit('gameStart',sID)
+        states[sID].o.emit('gameStart',sID)
+        turnUpdate('X',0,sID)
+    }
+
+    socket.on('turnPlayed',(pos,sID) => {
+        if (socket == states[sID].turn) {
+            let newPos = (1 * pos) + (35 - (7 * states[sID].columnSize[pos]))
+            states[sID].squares[newPos] = states[sID].turnSymbol
+            states[sID].columnSize[pos]++
+            states[sID].x.emit('squaresUpdate',states[sID].squares,states[sID].columnSize)
+            states[sID].o.emit('squaresUpdate',states[sID].squares,states[sID].columnSize)
+            turnUpdate(states[sID].notTurnSymbol,checkWin(sID),sID)
+        }
+    })
+
+    socket.on('disconnect',() => {
+        states.forEach((s,i) => {
+            if (s.x == socket) {
+                console.log('State ID:',i,', X Left')
+                if (s.o != '') {
+                    s.o.emit('status','Your opponent has left. Please refresh page for a new opponent.')
+                    states[i].o.disconnect()
+                }
+                playerCount--
+                states[i] = []
+            }
+            else if (s.o == socket) {
+                console.log('State ID:',i,', O Left')
+                if (s.x != '') {
+                    s.x.emit('status','Your opponent has left. Please refresh page for a new opponent.')
+                    states[i].x.disconnect()
+                }
+                playerCount--
+                states[i] = []
+            }
+        })
+    })
+})
